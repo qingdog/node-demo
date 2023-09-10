761e662dad88ea096398912faf0e3dd6341f0fed
qingdog <1759765836@qq.com>
2023-09-10 15:32:15 +0800

    vercel

--- api/index.js
+++ api/index.js
@@ -20,6 +20,7 @@ app.get('/api', (req, res) => {
     res.json({'secret': baseApi});
 });
 
+// 配置vercel重写以下api请求。"rewrites": [{ "source": "/api/(.*)", "destination": "/api" }]
 app.get("/api/item/:slug", (req, res) => {
     const {slug} = req.params;
     res.end(`Item: ${slug}`);
