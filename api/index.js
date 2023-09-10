c150fcf7068bea7702c49f108289cfd6960b017b
qingdog <1759765836@qq.com>
2023-09-10 15:04:48 +0800

    vercel

--- api/index.js
+++ api/index.js
@@ -25,13 +25,13 @@ app.get("/api/item/:slug", (req, res) => {
     res.end(`Item: ${slug}`);
 });
 
-app.get("/api/chat2", (req, res) => {
-    res.end(`Item: `);
+app.get("/api/chat", (req, res) => {
+    res.end('this is index chat!');
 });
 
 // const chat = require('./chat')
 // app.use('/api', chat);
 
-app.get("/api/chat", (req, res) => {
-    res.end("this is chat2!");
-});
+// app.get("/api/chat", (req, res) => {
+//     res.end("this is chat2!");
+// });
