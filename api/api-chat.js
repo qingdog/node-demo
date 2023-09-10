f4a9aa926d8e5936654365597e11f0629e6f9636
qingdog <1759765836@qq.com>
2023-09-10 19:40:20 +0800

    vercel

--- api/api-chat.js
+++ api/api-chat.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get("/chat", (req, res) => {
+router.get("/", (req, res) => {
     res.end("this is chat2!");
 });
 
-router.get("/chat/hello", (req, res) => {
+router.get("/hello", (req, res) => {
     res.end("hello chat!");
 });
 
