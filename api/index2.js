c8feed7ab862faee0be3d4344b358629184a3331
qingdog <1759765836@qq.com>
2023-09-11 20:06:34 +0800

    v1

--- api/index2.js
+++ api/index2.js
@@ -38,14 +38,8 @@ app.listen(port, () => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// /api路径可作为 当前文件index.js 服务器less函数重写的访问路径
+// 这里的/api路径可作为 当前目录api下的文件index.js 服务器less函数重写的访问路径
 app.get('/api', (req, res) => {
-    res.setHeader('Content-Type', 'application/json;charset=utf-8');
-    const chatApi = process.env.ENV_CHAT_API;
-    res.json({'secret': chatApi});
-});
-
-app.get('/api2', (req, res) => {
     res.setHeader('Content-Type', 'application/json;charset=utf-8');
     const chatApi = process.env.ENV_CHAT_API;
     res.json({'secret': chatApi});
