cfbfe4d53fb3406234d671fece68c0222246053f
qingdog <1759765836@qq.com>
2023-09-12 21:02:15 +0800

    test-hei

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.all('*', (_, res, next) => {
 
 // 开发环境
 const {createProxyMiddleware} = require('http-proxy-middleware');
-app.use('/v1', createProxyMiddleware({target: 'https://api.chatanywhere.com.cn', changeOrigin: true}));
+app.use('/proxy', createProxyMiddleware({target: 'https://localhost:8080', changeOrigin: true}));
 app.use(express.static('./'))
 
 // 导入 env
@@ -31,7 +31,7 @@ const envFiles = [
     '.env'              // 默认配置
 ];
 for (const envFile of envFiles) {
-    dotenv.config({ path: envFile });
+    dotenv.config({path: envFile});
 }
 
 let port = process.env.ENV_DEV_PORT || 3000;
@@ -41,7 +41,7 @@ app.listen(port, () => {
 
 // 使用 express.json() 中间件来解析 JSON 请求体
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 // 这里的/api路径可作为/api/index 即当前目录api下的文件index.js 服务器less函数重写的访问路径
 app.get('/api', (req, res) => {
@@ -53,3 +53,44 @@ app.get('/api', (req, res) => {
 // 配置vercel重写以下api请求。"rewrites": [{ "source": "/v1(.*)", "destination": "/api/index.js" }]
 const v1 = require('../router/v1.js')
 app.use('/v11', v1);
+
+let resData = {
+    "id": "chatcmpl-7xx584ZRr5PxiHRVZKlqYx7vKuRsM",
+    "object": "chat.completion.chunk",
+    "created": 1694522818,
+    "model": "gpt-3.5-turbo-0301",
+    "choices": [
+        {
+            "index": 0,
+            "delta": {
+                "role": "assistant",
+                "content": "嘿"
+            },
+            "finish_reason": null
+        }
+    ]
+}
+
+app.post('/v1/chat/completions', (req, res) => {
+    const message = "123456789"
+    res.set({
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+    });
+    let step = 0;
+    const time = setInterval(() => {
+        res.write(`data: ${JSON.stringify(resData)}\n\n`);
+        if (step === 10) {
+
+            res.write(`data: ${JSON.stringify(data2)}\n\n`);
+
+            res.end()
+            clearInterval(time)
+        }
+        step++;
+    }, 50);
+});
+
+let data2 = {"id":"chatcmpl-7xxHoj8mAdfvCZISNtptJxwExaYYW","object":"chat.completion.chunk","created":1694523604,"model":"gpt-3.5-turbo-0301","choices":[{"index":0,"delta":{},"finish_reason":"stop"}]}
+let data3 = '[DONE]'
